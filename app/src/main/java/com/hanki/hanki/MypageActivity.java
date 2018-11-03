a4780373fdc66cbeb05f3ed4263940ab6b267bb7
jooyeong <young951109@naver.com>
2018-11-03 15:28:36 +0900

    MypageActivity에 스위치 버튼 추가

--- app/src/main/java/com/hanki/hanki/MypageActivity.java
+++ app/src/main/java/com/hanki/hanki/MypageActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MypageActivity extends AppCompatActivity {
 
-    Button mypage_saveBtn;
+    Switch emailSwitch;
+    Switch smsSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +22,38 @@ public class MypageActivity extends AppCompatActivity {
     }
 
     public void init() {
-        mypage_saveBtn = (Button) findViewById(R.id.mypage_saveBtn);
+        emailSwitch = (Switch) findViewById(R.id.emailSwitch);
+        smsSwitch = (Switch) findViewById(R.id.smsSwitch);
+
+        emailSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MypageActivity.this, "email", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }
+        });
+
+        smsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(MypageActivity.this, "sms", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
+            }
+        });
     }
 
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.mypage_saveBtn: //저장버튼
                 finish();
+                break;
+            case R.id.mypage_withdrawalBtn: //회원탈퇴버튼
                 break;
         }
     }
