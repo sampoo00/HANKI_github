1d8abf838c877c0308e56118d1850389751c81aa
nayeon <ceoahn95@gmail.com>
2018-11-30 19:48:22 +0900

    splash에서 permission확인

--- app/src/main/java/com/hanki/hanki/SplashActivity.java
+++ app/src/main/java/com/hanki/hanki/SplashActivity.java
@@ -9,16 +9,22 @@ import android.os.Bundle;
 import com.hanki.hanki.Tutorial.TutorialActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
+    boolean isFirstRun;
+    String checkShowTutorial;
     Handler handler = new Handler();
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-//        SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
-//        final int checkShowTutorial = pref.getInt("checkTutorial", 0);
+        pref = getSharedPreferences("pref", MODE_PRIVATE);
+
+        editor = pref.edit();
+        isFirstRun = pref.getBoolean("isFirstRun", true);
+        checkShowTutorial = pref.getString("selectedNoShow", "no");
 
         handler.postDelayed(new Runnable() {
             @Override
@@ -32,9 +38,26 @@ public class SplashActivity extends AppCompatActivity {
 //                startActivity(intent);
 //                finish();
 
-                Intent intent = new Intent(SplashActivity.this, PermissionActivity.class);
-                startActivity(intent);
-                finish();
+                if(isFirstRun){
+                    editor.putBoolean("isFirstRun", false);
+                    editor.apply();
+                    Intent intent = new Intent(SplashActivity.this, PermissionActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    if(checkShowTutorial.equals("no")) {
+                        Intent intent = new Intent(SplashActivity.this, TutorialActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else{
+                        Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+
             }
         }, 3000);
     }
