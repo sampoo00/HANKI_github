8d2de46f2416bbaab89ee564e7ab68c3e19db341
nayeon <ceoahn95@gmail.com>
2018-12-07 17:23:11 +0900

    splash 틀 수정

--- app/src/main/java/com/hanki/hanki/SplashActivity.java
+++ app/src/main/java/com/hanki/hanki/SplashActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.hanki.hanki.Tutorial.TutorialActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -23,8 +24,13 @@ public class SplashActivity extends AppCompatActivity {
         pref = getSharedPreferences("pref", MODE_PRIVATE);
 
         editor = pref.edit();
+        //초기 값 지정
         isFirstRun = pref.getBoolean("isFirstRun", true);
         checkShowTutorial = pref.getString("selectedNoShow", "no");
+
+        LottieAnimationView lottie = (LottieAnimationView) findViewById(R.id.splash_lottie);
+        lottie.playAnimation();
+        lottie.cancelAnimation();
 
         handler.postDelayed(new Runnable() {
             @Override
