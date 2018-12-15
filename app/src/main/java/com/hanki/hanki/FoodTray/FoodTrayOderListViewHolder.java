bf430842b1399eeda1e3d566e6a9c6e4c4d0f0c3
nayeon <ceoahn95@gmail.com>
2018-12-16 04:05:55 +0900

    식판 Recycler UI 생성

--- app/src/main/java/com/hanki/hanki/FoodTray/FoodTrayOderListViewHolder.java
+++ app/src/main/java/com/hanki/hanki/FoodTray/FoodTrayOderListViewHolder.java
@@ -2,9 +2,15 @@ package com.hanki.hanki.FoodTray;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class FoodTrayOderListViewHolder extends RecyclerView.ViewHolder {
+    //매장명, 버튼
+    TextView VH_ShopName;
+    ImageView VH_deleteBtn;
+
 
 
     public FoodTrayOderListViewHolder(View itemView) {
