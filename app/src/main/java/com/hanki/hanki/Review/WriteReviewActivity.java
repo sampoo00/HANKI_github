030f8ae7738e4efa8925ab93a7bf1ca215712102
jooyeong <young951109@naver.com>
2018-12-18 17:48:10 +0900

    리뷰작성 날짜 포맷 추가

--- app/src/main/java/com/hanki/hanki/Review/WriteReviewActivity.java
+++ app/src/main/java/com/hanki/hanki/Review/WriteReviewActivity.java
@@ -1,5 +1,6 @@
 package com.hanki.hanki.Review;
 
+import android.content.Context;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,8 +8,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.hanki.hanki.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class WriteReviewActivity extends AppCompatActivity {
 
@@ -25,10 +32,15 @@ public class WriteReviewActivity extends AppCompatActivity {
         setSupportActionBar(review_toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowCustomEnabled(true);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView writeReview_date = (TextView) findViewById(R.id.writeReview_date);
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+        writeReview_date.setText(sdf.format(now));
 
         Button review_saveBtn = (Button) findViewById(R.id.review_saveBtn);
-        review_saveBtn.setOnClickListener(new View.OnClickListener() {
+        review_saveBtn.setOnClickListener(new View.OnClickListener() { //확인 버튼 클릭 시
             @Override
             public void onClick(View v) {
                 finish();
@@ -44,5 +56,10 @@ public class WriteReviewActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
